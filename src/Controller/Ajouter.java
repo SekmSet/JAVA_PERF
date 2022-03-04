6d5e6129472862efaeb96177f6eb690d014dd949
Priscilla JOLY <priscilla.joly@epitech.eu>
2022-03-04 13:03:51 +0100

    refacto organization

---
+++ src/Controller/Ajouter.java
@@ -0,0 +1,364 @@
+package Controller;
+
+import Model.Occasion;
+import Model.Voiture;
+import net.miginfocom.swing.MigLayout;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Objects;
+
+import static java.lang.Float.parseFloat;
+import static java.lang.Integer.parseInt;
+/*
+ * Created by JFormDesigner on Tue Mar 01 09:19:45 CET 2022
+ */
+
+/**
+ * @author Joly
+ */
+public class Ajouter extends JFrame {
+
+    private final String colorRed = "#CD1818";
+    public static Home home;
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // Generated using JFormDesigner Evaluation license - Joly
+    private JLabel label1;
+    private JButton button2;
+    private JLabel label9;
+    private JTextField textField8;
+    private JLabel label12;
+    private JLabel span16;
+    private JLabel label2;
+    private JTextField textField1;
+    private JLabel span9;
+    private JLabel label3;
+    private JTextField textField2;
+    private JLabel span10;
+    private JLabel label4;
+    private JTextField textField3;
+    private JLabel span11;
+    private JLabel label5;
+    private JTextField textField4;
+    private JLabel span12;
+    private JLabel label6;
+    private JTextField textField5;
+    private JLabel span13;
+    private JLabel label7;
+    private JTextField textField6;
+    private JLabel label10;
+    private JLabel span14;
+    private JLabel label8;
+    private JTextField textField7;
+    private JLabel label11;
+    private JLabel span15;
+    private JButton button1;
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            try {
+                Ajouter frame = new Ajouter();
+                frame.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+    public Ajouter() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        // Generated using JFormDesigner Evaluation license - Joly
+        label1 = new JLabel();
+        button2 = new JButton();
+        label9 = new JLabel();
+        textField8 = new JTextField();
+        label12 = new JLabel();
+        span16 = new JLabel();
+        label2 = new JLabel();
+        textField1 = new JTextField();
+        span9 = new JLabel();
+        label3 = new JLabel();
+        textField2 = new JTextField();
+        span10 = new JLabel();
+        label4 = new JLabel();
+        textField3 = new JTextField();
+        span11 = new JLabel();
+        label5 = new JLabel();
+        textField4 = new JTextField();
+        span12 = new JLabel();
+        label6 = new JLabel();
+        textField5 = new JTextField();
+        span13 = new JLabel();
+        label7 = new JLabel();
+        textField6 = new JTextField();
+        label10 = new JLabel();
+        span14 = new JLabel();
+        label8 = new JLabel();
+        textField7 = new JTextField();
+        label11 = new JLabel();
+        span15 = new JLabel();
+        button1 = new JButton();
+
+        //======== this ========
+        var contentPane = getContentPane();
+        contentPane.setLayout(new MigLayout(
+            "hidemode 3",
+            // columns
+            "[184,fill]" +
+            "[264,fill]" +
+            "[145,fill]",
+            // rows
+            "[]" +
+            "[]" +
+            "[]" +
+            "[]" +
+            "[]" +
+            "[]" +
+            "[]" +
+            "[]" +
+            "[]" +
+            "[]" +
+            "[]" +
+            "[]" +
+            "[]" +
+            "[]" +
+            "[]" +
+            "[]" +
+            "[]" +
+            "[]" +
+            "[108]"));
+
+        //---- label1 ----
+        label1.setText("GARAGE VAULVAU");
+        label1.setFont(label1.getFont().deriveFont(label1.getFont().getStyle() | Font.BOLD, label1.getFont().getSize() + 10f));
+        label1.setHorizontalAlignment(SwingConstants.CENTER);
+        label1.setForeground(new Color(0, 0, 153));
+        contentPane.add(label1, "cell 0 0 2 1");
+
+        //---- button2 ----
+        button2.setText("Quitter");
+        button2.addActionListener(e -> quitterAddButton(e));
+        contentPane.add(button2, "cell 2 0");
+
+        //---- label9 ----
+        label9.setText("Immatriculation");
+        label9.setFont(label9.getFont().deriveFont(label9.getFont().getStyle() | Font.BOLD, label9.getFont().getSize() + 3f));
+        contentPane.add(label9, "cell 0 1");
+        contentPane.add(textField8, "cell 1 1");
+
+        //---- label12 ----
+        label12.setText("ABC-DEF00");
+        contentPane.add(label12, "cell 2 1");
+        contentPane.add(span16, "cell 0 2 2 1");
+
+        //---- label2 ----
+        label2.setText("Marque");
+        label2.setFont(label2.getFont().deriveFont(label2.getFont().getStyle() | Font.BOLD, label2.getFont().getSize() + 3f));
+        contentPane.add(label2, "cell 0 3");
+        contentPane.add(textField1, "cell 1 3");
+
+        //---- span9 ----
+        span9.setFont(span9.getFont().deriveFont(span9.getFont().getStyle() | Font.ITALIC, span9.getFont().getSize() + 1f));
+        contentPane.add(span9, "cell 0 4 3 1");
+
+        //---- label3 ----
+        label3.setText("Mod\u00e8le");
+        label3.setFont(label3.getFont().deriveFont(label3.getFont().getStyle() | Font.BOLD, label3.getFont().getSize() + 3f));
+        contentPane.add(label3, "cell 0 5");
+        contentPane.add(textField2, "cell 1 5");
+
+        //---- span10 ----
+        span10.setFont(span10.getFont().deriveFont(span10.getFont().getStyle() | Font.ITALIC, span10.getFont().getSize() + 1f));
+        contentPane.add(span10, "cell 0 6 3 1");
+
+        //---- label4 ----
+        label4.setText("Kilom\u00e9trage");
+        label4.setFont(label4.getFont().deriveFont(label4.getFont().getStyle() | Font.BOLD, label4.getFont().getSize() + 3f));
+        contentPane.add(label4, "cell 0 7");
+        contentPane.add(textField3, "cell 1 7");
+
+        //---- span11 ----
+        span11.setFont(span11.getFont().deriveFont(span11.getFont().getStyle() | Font.ITALIC, span11.getFont().getSize() + 1f));
+        contentPane.add(span11, "cell 0 8 3 1");
+
+        //---- label5 ----
+        label5.setText("Prix");
+        label5.setFont(label5.getFont().deriveFont(label5.getFont().getStyle() | Font.BOLD, label5.getFont().getSize() + 3f));
+        contentPane.add(label5, "cell 0 9");
+        contentPane.add(textField4, "cell 1 9");
+
+        //---- span12 ----
+        span12.setFont(span12.getFont().deriveFont(span12.getFont().getStyle() | Font.ITALIC, span12.getFont().getSize() + 1f));
+        contentPane.add(span12, "cell 0 10 3 1");
+
+        //---- label6 ----
+        label6.setText("Dur\u00e9e de vie");
+        label6.setFont(label6.getFont().deriveFont(label6.getFont().getStyle() | Font.BOLD, label6.getFont().getSize() + 3f));
+        contentPane.add(label6, "cell 0 11");
+        contentPane.add(textField5, "cell 1 11");
+
+        //---- span13 ----
+        span13.setFont(span13.getFont().deriveFont(span13.getFont().getStyle() | Font.ITALIC, span13.getFont().getSize() + 1f));
+        contentPane.add(span13, "cell 0 12 3 1");
+
+        //---- label7 ----
+        label7.setText("Date premier achat");
+        label7.setFont(label7.getFont().deriveFont(label7.getFont().getStyle() | Font.BOLD, label7.getFont().getSize() + 3f));
+        contentPane.add(label7, "cell 0 13");
+        contentPane.add(textField6, "cell 1 13");
+
+        //---- label10 ----
+        label10.setText("JJ/MM/YYYY");
+        contentPane.add(label10, "cell 2 13");
+
+        //---- span14 ----
+        span14.setFont(span14.getFont().deriveFont(span14.getFont().getStyle() | Font.ITALIC, span14.getFont().getSize() + 1f));
+        contentPane.add(span14, "cell 0 14 3 1");
+
+        //---- label8 ----
+        label8.setText("Date revente");
+        label8.setFont(label8.getFont().deriveFont(label8.getFont().getStyle() | Font.BOLD, label8.getFont().getSize() + 3f));
+        contentPane.add(label8, "cell 0 15");
+        contentPane.add(textField7, "cell 1 15");
+
+        //---- label11 ----
+        label11.setText("JJ/MM/YYYY");
+        contentPane.add(label11, "cell 2 15");
+
+        //---- span15 ----
+        span15.setFont(span15.getFont().deriveFont(span15.getFont().getStyle() | Font.ITALIC, span15.getFont().getSize() + 1f));
+        contentPane.add(span15, "cell 0 16 3 1");
+
+        //---- button1 ----
+        button1.setText("VALIDER");
+        button1.addActionListener(e -> {
+            try {
+                addVehiculeButton(e);
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            } catch (ParseException ex) {
+                ex.printStackTrace();
+            }
+        });
+        contentPane.add(button1, "cell 0 18 3 1");
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    private void quitterAddButton(ActionEvent e) {
+        home = new Home();
+        home.setVisible(true);
+        this.setVisible(false);
+    }
+
+    private boolean addVehiculeButton(ActionEvent e) throws SQLException, ParseException {
+        String immatriculation = textField8.getText();
+        String marque = textField1.getText();
+        String modele = textField2.getText();
+
+        String kilometrage = textField3.getText();
+        String dureeDeVie = textField5.getText();
+
+        String prix = textField4.getText();
+
+        String datePremierAchat = textField6.getText();
+        String dateRevente = textField6.getText();
+
+        boolean error = false;
+
+        if(immatriculation.equals("")) {
+            span16.setText("Champ obligatoire.");
+            span9.setForeground(Color.decode(this.colorRed));
+
+            error = true;
+        }
+
+        if(marque.equals("")) {
+            span9.setText("Champ obligatoire.");
+            span9.setForeground(Color.decode(this.colorRed));
+
+            error = true;
+        }
+
+        if(!Objects.equals(kilometrage, "0") && (datePremierAchat.equals("") || dateRevente.equals(""))) {
+            span14.setText("Champ obligatoire.");
+            span15.setText("Champ obligatoire.");
+
+            span14.setForeground(Color.decode(this.colorRed));
+            span15.setForeground(Color.decode(this.colorRed));
+
+            error = true;
+        } else if(modele.equals("")) {
+            span10.setText("Champ obligatoire.");
+            span10.setForeground(Color.decode(this.colorRed));
+
+            error = true;
+        } else if (kilometrage.equals("")) {
+            span11.setText("Champ obligatoire.");
+            span11.setForeground(Color.decode(this.colorRed));
+
+            error = true;
+        } else if (prix.equals("")) {
+            span12.setText("Champ obligatoire.");
+            span12.setForeground(Color.decode(this.colorRed));
+
+            error = true;
+        }
+
+        if (error) {
+            return false;
+        }
+
+        if (kilometrage.equals("0")) {
+            Voiture v = new Voiture(
+                    immatriculation,
+                    marque,
+                    modele,
+                    parseInt(kilometrage),
+                    parseFloat(prix)
+            );
+
+            v.createVoiture(v);
+        } else {
+            Date convertToDatePremierAchat = new SimpleDateFormat("dd/MM/yyyy").parse(datePremierAchat);
+            Date convertToDateRevente = new SimpleDateFormat("dd/MM/yyyy").parse(dateRevente);
+
+            System.out.println(convertToDatePremierAchat);
+            System.out.println(convertToDateRevente);
+
+            Occasion o = new Occasion(
+                    immatriculation,
+                    marque,
+                    modele,
+                    parseInt(kilometrage),
+                    parseFloat(prix),
+                    parseInt(dureeDeVie),
+                    convertToDatePremierAchat,
+                    convertToDateRevente
+            );
+
+            o.createOccasion(o);
+        }
+
+        textField1.setText("");
+        textField2.setText("");
+        textField3.setText("");
+        textField4.setText("");
+        textField5.setText("");
+        textField6.setText("");
+
+        return true;
+    }
+}
---
+++ src/Controller/Voir.java
@@ -0,0 +1,369 @@
+package Controller;
+
+import java.awt.event.*;
+
+import Model.Occasion;
+import Model.Voiture;
+import net.miginfocom.swing.MigLayout;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.awt.event.KeyEvent;
+
+/*
+ * Created by JFormDesigner on Tue Mar 01 09:19:55 CET 2022
+ */
+/**
+ * @author Joly
+ */
+public class Voir extends JFrame {
+    ArrayList<Voiture> listNeufs;
+    ArrayList<Occasion> listOccasions;
+    Voiture neuf = null;
+    Occasion occasion = null;
+    String type;
+    String currentImmatriculation;
+
+    public static Home home;
+
+    int index = 0;
+
+    private final String colorRed = "#CD1818";
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // Generated using JFormDesigner Evaluation license - Joly
+    private JLabel label1;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JScrollPane scrollPane1;
+    private JList<String> list1;
+    private JLabel spanError;
+    private JButton button4;
+    private DefaultListModel listModel;
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            try {
+                Voir frame = new Voir();
+                frame.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    public Voir() throws SQLException {
+        initComponents();
+    }
+
+    private void list1KeyPressed(KeyEvent e) {
+
+        occasion = null;
+        neuf = null;
+
+        int keyCode = e.getKeyCode();
+
+        var listWithNeufAndOccasions = new ArrayList<>();
+        listWithNeufAndOccasions.addAll(listNeufs);
+        listWithNeufAndOccasions.addAll(listOccasions);
+
+        if (keyCode == KeyEvent.VK_RIGHT) {
+            if (type.equals("list occasion")) {
+                index += 1;
+                if (listOccasions.size() == index) {
+                    index = 0;
+                }
+
+                occasion = listOccasions.get(index);
+                currentImmatriculation = neuf.getImmatriculation();
+
+            } else if (type.equals("list neuf")) {
+                index += 1;
+                if (listNeufs.size() == index) {
+                    index = 0;
+                }
+
+                neuf = listNeufs.get(index);
+                currentImmatriculation = neuf.getImmatriculation();
+            }  else if (type.equals("list all")) {
+                index += 1;
+                if (listWithNeufAndOccasions.size() == index) {
+                    index = 0;
+                }
+
+                getNeufOfOccasion(listWithNeufAndOccasions);
+            }
+        } else if (keyCode == KeyEvent.VK_LEFT) {
+            switch (type) {
+                case "list occasion" -> {
+                    System.out.println("oc");
+                    index -= 1;
+                    if (index == 0) {
+                        index = listOccasions.size() - 1;
+                    }
+                    occasion = listOccasions.get(index);
+                    currentImmatriculation = neuf.getImmatriculation();
+                }
+                case "list neuf" -> {
+                    index -= 1;
+                    if (index < 0) {
+                        index = listNeufs.size() - 1;
+                    }
+                    neuf = listNeufs.get(index);
+                    currentImmatriculation = neuf.getImmatriculation();
+                }
+                case "list all" -> {
+                    System.out.println("ne");
+                    index -= 1;
+                    if (index < 0) {
+                        index = listWithNeufAndOccasions.size() - 1;
+                    }
+                    getNeufOfOccasion(listWithNeufAndOccasions);
+                }
+            }
+
+        } else {
+            System.out.println("Non reconnu");
+        }
+
+        listModel = new DefaultListModel();
+
+        if(neuf != null) {
+            listModel.addElement(neuf);
+        }
+
+        if(occasion != null) {
+            listModel.addElement(occasion);
+        }
+
+        list1.setModel(listModel);
+    }
+
+    private void getNeufOfOccasion(ArrayList<Object> listWithNeufAndOccasions) {
+        var neufOfOccasions = listWithNeufAndOccasions.get(index);
+        if (neufOfOccasions.getClass() == Voiture.class) {
+            neuf = (Voiture)neufOfOccasions;
+            currentImmatriculation = neuf.getImmatriculation();
+        }
+        if (neufOfOccasions.getClass() == Occasion.class) {
+            occasion = (Occasion)neufOfOccasions;
+            currentImmatriculation = occasion.getImmatriculation();
+        }
+    }
+
+    private void initComponents() throws SQLException {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        // Generated using JFormDesigner Evaluation license - Joly
+        label1 = new JLabel();
+        button1 = new JButton();
+        button2 = new JButton();
+        button3 = new JButton();
+        scrollPane1 = new JScrollPane();
+        list1 = new JList<>();
+        spanError = new JLabel();
+        button4 = new JButton();
+
+        //======== this ========
+        var contentPane = getContentPane();
+        contentPane.setLayout(new MigLayout(
+            "hidemode 3",
+            // columns
+            "[fill]" +
+            "[fill]" +
+            "[293,fill]" +
+            "[218,fill]",
+            // rows
+            "[]" +
+            "[]" +
+            "[]" +
+            "[]" +
+            "[]" +
+            "[]" +
+            "[]"));
+
+        //---- label1 ----
+        label1.setText("GARAGE VAULVAU");
+        label1.setFont(label1.getFont().deriveFont(label1.getFont().getStyle() | Font.BOLD, label1.getFont().getSize() + 10f));
+        label1.setHorizontalAlignment(SwingConstants.CENTER);
+        label1.setForeground(new Color(0, 0, 153));
+        contentPane.add(label1, "cell 0 0 3 1");
+
+        //---- button1 ----
+        button1.setText("QUITTER");
+        button1.addActionListener(e -> quitterListButton(e));
+        contentPane.add(button1, "cell 3 0");
+
+        //---- button2 ----
+        button2.setText("Neuve");
+        button2.addActionListener(e -> {
+            try {
+                voirVoitureNeuve(e);
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        });
+        contentPane.add(button2, "cell 2 1");
+
+        //---- button3 ----
+        button3.setText("Occasion");
+        button3.addActionListener(e -> {
+            try {
+                voirVoitureOccasion(e);
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        });
+        contentPane.add(button3, "cell 2 2");
+
+        //======== scrollPane1 ========
+        {
+
+            //---- list1 ----
+            list1.setModel(new AbstractListModel<String>() {
+                String[] values = {
+                    "i",
+                    "\u00a7"
+                };
+                @Override
+                public int getSize() { return values.length; }
+                @Override
+                public String getElementAt(int i) { return values[i]; }
+            });
+            list1.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+            list1.addKeyListener(new KeyAdapter() {
+                @Override
+                public void keyPressed(KeyEvent e) {
+                    list1KeyPressed(e);
+                }
+            });
+            scrollPane1.setViewportView(list1);
+        }
+        contentPane.add(scrollPane1, "cell 2 4 2 1");
+
+        //---- spanError ----
+        spanError.setFont(spanError.getFont().deriveFont(spanError.getFont().getStyle() | Font.ITALIC, spanError.getFont().getSize() + 3f));
+        contentPane.add(spanError, "cell 2 5 2 1");
+
+        //---- button4 ----
+        button4.setText("Voir les d\u00e9tails");
+        button4.addActionListener(e -> voirDetailsButton(e));
+        contentPane.add(button4, "cell 2 6 2 1");
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+        getAll();
+    }
+
+    private void quitterListButton(ActionEvent e) {
+        home = new Home();
+        home.setVisible(true);
+        this.setVisible(false);
+    }
+
+    private void voirDetailsButton(ActionEvent e) {
+        index = list1.getSelectedIndex();
+
+        if(index == -1) {
+            spanError.setText("Aucun élément sélectionner.");
+            spanError.setForeground(Color.decode(this.colorRed));
+            return;
+        }
+
+        Voiture v = new Voiture();
+        Occasion o = new Occasion();
+
+        var element = listModel.getElementAt(index);
+
+        switch (type) {
+            case "list all" -> {
+                occasion = o.getVoitureNeufOrOccasion(listOccasions, element.toString());
+                neuf = v.getVoitureNeufOrOccasion(listNeufs, element.toString());
+                if (occasion != null) {
+                    currentImmatriculation = occasion.getImmatriculation();
+                } else if (neuf != null) {
+                    currentImmatriculation = neuf.getImmatriculation();
+                }
+            }
+            case "list occasion" -> {
+                occasion = o.getVoitureOccasion(listOccasions, element.toString());
+                currentImmatriculation = occasion.getImmatriculation();
+            }
+            case "list neuf" -> {
+                neuf = v.getVoitureNeuf(listNeufs, element.toString());
+                currentImmatriculation = neuf.getImmatriculation();
+            }
+        }
+
+        listModel = new DefaultListModel();
+
+        if(neuf != null) {
+            listModel.addElement(neuf);
+        }
+
+        if(occasion != null) {
+            listModel.addElement(occasion);
+        }
+
+        list1.setModel(listModel);
+    }
+
+    private void voirVoitureNeuve(ActionEvent e) throws SQLException {
+        Voiture voitures = new Voiture();
+        listNeufs = voitures.getVoituresNeufs();
+
+        if(listNeufs != null) {
+            listModel = new DefaultListModel();
+
+            for(Voiture v : listNeufs) {
+                listModel.addElement(v.getImmatriculation());
+            }
+
+            list1.setModel(listModel);
+
+            type = "list neuf";
+        }
+    }
+
+    private void voirVoitureOccasion(ActionEvent e) throws SQLException {
+        Occasion occasions = new Occasion();
+        listOccasions = occasions.getVoituresOccasions();
+
+        if(listOccasions != null) {
+            listModel = new DefaultListModel();
+
+            for(Occasion o : listOccasions) {
+                listModel.addElement(o.getImmatriculation());
+            }
+
+            list1.setModel(listModel);
+            type = "list occasion";
+        }
+    }
+
+    private void getAll () throws SQLException {
+        Voiture voitures = new Voiture();
+        Occasion occasions = new Occasion();
+        listNeufs = voitures.getVoituresNeufs();
+        listOccasions = occasions.getVoituresOccasions();
+        listModel = new DefaultListModel();
+
+        if(listNeufs != null && listOccasions != null) {
+
+            for(Voiture v : listNeufs) {
+                listModel.addElement(v.getImmatriculation());
+            }
+            list1.setModel(listModel);
+
+            for(Occasion o : listOccasions) {
+                listModel.addElement(o.getImmatriculation());
+            }
+            list1.setModel(listModel);
+            type = "list all";
+        }
+    }
+}
